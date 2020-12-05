bfc524e547eb60fff7a916c98f0f36ee1b5b8d82
Kunal <kunal.rohitas29@gmail.com>
2020-12-05 14:52:35 +0530

    Change 2 , made the questions boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,49 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex += 1;
+    print(questionIndex);
+  }
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My First App'),),
-      body: Text('This is my default text'),
-    ),)
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: () => {print('Answer 2 chosen!')},
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: () {
+                print('Answer 3 chosen!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer4'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
